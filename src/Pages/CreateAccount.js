cff119d3bc6cae8fc433032e92514e5acb619ad6
Garance <danvin.garance@gmail.com>
2024-01-27 18:12:00 -0500

    CreateAccounts.js done rectified few issues in Connect.js chnaged a name in CreateAccount.css

--- src/Pages/CreateAccount.js
+++ src/Pages/CreateAccount.js
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react';
+import '../Styling/CreateAccount.css';
+import { Container, Button } from 'react-bootstrap';
 
 export default function CreateAccount() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(username, password);
+  };
+
   return (
-    <div>
-      id
-    </div>
-  )
+    <Container>
+      <div>
+        <div id='Logo' />
+        <p id='WelcomeMessage'>
+          Congratulations on taking the first step!
+          <br/>
+          Your journey to mastering sign language begins now. Let's sign together!
+        </p>
+        <form onSubmit={handleSubmit}>
+          <div id='UsernameLabel'> Username</div>
+          <input
+            id='UsernameInput'
+            type='text'
+            value={username}
+            onChange={handleUsernameChange}
+          />
+          <div id='PasswordLabel'> Password </div>
+          <input
+            id='PasswordInput'
+            type='password'
+            value={password}
+            onChange={handlePasswordChange}
+          />
+          <Button id='BoutonCreateConnect' type='submit'>
+            Create Account
+          </Button>
+        </form>
+        <div>
+          <p id ='EncouragementMessage'>You're now part of a world where hands brong words to life. </p>
+        </div>
+      </div>
+
+    </Container>
+  );
 }
